4c3d8daa2814ad1bca3708776c990fbf1c9a99b5
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-02-02 14:29:59 -0800

    Delete course is working now. Youtube video: 65/97.

---
+++ src/components/CourseManagerComponent.js
@@ -0,0 +1,61 @@
+import React from "react";
+import CourseTableComponent from "./CourseTableComponent";
+import CourseGridComponent from "./CourseGridComponent";
+
+class CourseManagerComponent extends React.Component {
+    state = {
+        layout: 'table',
+        courses: [
+            {_id: '123', title: 'Course A'},
+            {_id: '234', title: 'Course B'},
+            {_id: '356', title: 'Course C'},
+            {_id: '456', title: 'Course D'},
+            {_id: '567', title: 'Course E'}
+        ]
+    }
+
+    toggle = () => {
+        this.setState(prevState => {
+            if(prevState.layout === 'table') {
+                return ({layout: 'grid'})
+            } else {
+                return ({layout: 'table'})
+            }
+        })
+    }
+
+    deleteCourse = (course) => {
+        console.log(course)
+        this.setState(prevState => {
+            return ({
+                courses: prevState
+                    .courses
+                    .filter(function(crs) {
+                    return crs._id !== course._id
+                })
+            })
+        })
+    }
+
+
+    render() {
+        return(
+            <div>
+                <h1>Course Manager</h1>
+                <button onClick={this.toggle}>Toggle</button>
+                {
+                    this.state.layout === 'table' &&
+                    <CourseTableComponent
+                        deleteCourse={this.deleteCourse}
+                        courses={this.state.courses}/>
+                }
+                {
+                    this.state.layout === 'grid' &&
+                    <CourseGridComponent courses={this.state.courses}/>
+                }
+            </div>
+        )
+    }
+}
+
+export default CourseManagerComponent;

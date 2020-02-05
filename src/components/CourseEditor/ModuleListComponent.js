ee98a17a524556227c7ee38c8c6374c48d1e9bb4
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-02-05 15:50:22 -0800

    Module list is better.

--- src/components/CourseEditor/ModuleListComponent.js
+++ src/components/CourseEditor/ModuleListComponent.js
@@ -3,31 +3,19 @@ import "./CourseEditorComponent.css"
 
 const ModuleListComponent = ({modules}) =>
     <div>
-    <div className = "bg-dark" >
-        <div className = "list-group wbdv-module-list" style={{margin:"20px"}}>
-            <a href = "#" className = "list-group-item wbdv-module-item" style={{margin:"5px"}} >
-                <h6 className = "wbdv-module-item-title" > Module 1 - jQuery </h6>
-                <i className="fa fa-times fa-right wbdv-module-item-delete-btn"></i>
-            </a>
-            <a href="#" className="list-group-item wbdv-module-item active wbdv-selected" style={{margin:"5px"}}>
-                <h6 className="wbdv-module-item-title">Module 2 - React</h6>
-                <i className="fa fa-times fa-right wbdv-module-item-delete-btn"></i>
-            </a>
-        </div>
-    </div>
-        <div>
-        <ul className="bg-dark list-group wbdv-module-list">
-            {modules.map(module =>
-                <li key={module._id}
-                    className="wbdv-module-item">
-                    <span>
-                        {module.title}
-                        <i className="fa fa-times wbdv-module-item-delete-btn"></i>
+    <ul className="bg-dark list-group wbdv-module-list">
+        {modules.map(module =>
+            <li key={module._id}
+                className="wbdv-module-item">
+                <span>
+                    <span className="wbdv-module-item-title">
+                    {module.title}
                     </span>
-                </li>
-            )}
-        </ul>
-        </div>
+                    <i className="fa fa-times wbdv-module-item-delete-btn"></i>
+                </span>
+            </li>
+        )}
+    </ul>
     </div>
 
 export default ModuleListComponent

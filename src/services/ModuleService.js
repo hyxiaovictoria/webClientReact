1f87383743a36f2c2cdd41d5647358adffadba58
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-02-10 20:40:14 -0800

    Added Moduleservice.js.

---
+++ src/services/ModuleService.js
@@ -0,0 +1,36 @@
+import {API_URL} from "../constants";
+
+export const findModulesForCourse = async (courseId) => {
+    const response = await fetch(`${API_URL}/${courseId}/modules`)
+
+    return await response.json()
+}
+
+export const createModule = (courseId, module) =>
+    fetch(`${API_URL}/${courseId}`, {
+        method: 'POST',
+        body: JSON.stringify(module),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+
+export const updateModule = async (moduleId, module) => {
+    const response = await fetch(`${API_URL}/modules/${moduleId}`, {
+        method: 'PUT',
+        body: JSON.stringify(module),
+        headers: {
+            "content-type": 'application/json'
+        }
+    })
+
+    return await response.json()
+}
+
+export const deleteModule = async (moduleId) => {
+    const response = await fetch(`${API_URL}/modules/${moduleId}`, {
+        method: 'DELETE'
+    })
+
+    return await response.json()
+}

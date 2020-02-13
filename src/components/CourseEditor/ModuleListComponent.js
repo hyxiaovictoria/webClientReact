1932a46c3fd0acd110a178bad413904a8d428799
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-02-12 20:22:06 -0800

    Module session, edit can auto hide.

--- src/components/CourseEditor/ModuleListComponent.js
+++ src/components/CourseEditor/ModuleListComponent.js
@@ -1,6 +1,8 @@
 import React from "react";
-import ModuleListItem from "./ModuleListItem";
+//import ModuleListItem from "./ModuleListItem";
 import "./CourseEditorComponent.css"
+import {MODULES_API_URL} from "../../constants";
+import {connect} from "react-redux";
 
 //import "./CourseEditorComponent.css"
 //import {createStore} from "redux";
@@ -8,7 +10,7 @@ import "./CourseEditorComponent.css"
 //import createModule from "../../services/ModuleService"
 //import moduleReducer from "../../reducers/modules";
 
-export default class ModuleListComponent extends React.Component {
+class ModuleListComponent extends React.Component {
 
     componentDidMount() {
         this.props.findModulesForCourse(this.props.courseId)
@@ -24,33 +26,65 @@ export default class ModuleListComponent extends React.Component {
     }
 
     render() {
+        const active = true
         return (
             <ul className="bg-dark list-group wbdv-module-list">
                 {
-                    this.props.modules && this.props.modules.map(module =>
-                        <ModuleListItem
-                            key={module._id}
-                            edit={() => {
-                                const moduleId = module._id
-                                console.log('ModuleListComponent ' + this.props.courseId)
-                                this.props.history.push(`/course-editor/${this.props.courseId}/module/${moduleId}`)
-                                this.setState({
-                                    editingModuleId: module._id
-                                })
-                            }}
-                            select={() => {
-                                const moduleId = module._id
-                                this.props.history.push(`/course-editor/${this.props.courseId}/module/${moduleId}`)
-                                this.setState({
-                                    activeModuleId: module._id
-                                })
-                            }}
-                            save={() => this.setState({
-                                editingModuleId: ''
-                            })}
-                            editing={module._id === this.state.editingModuleId}
-                            active={module._id === this.state.activeModuleId}
-                            module={module}/>)
+                    this.props.modules && this.props.modules.map(
+                        module =>
+                            <li
+                                className={`wbdv-module-item ${active ? 'active':''}`}>
+                            <span className="wbdv-module-item-title">
+                            {module.title}
+                            </span>
+                            {/*{editing &&*/}
+                            <span className="fa-right-only-50">
+                                { this.state.editingModuleId !== module._id &&
+                                    <a onClick={
+                                        () => {
+                                            this.setState({
+                                                editingModuleId: module._id
+                                            })
+                                            console.log('Clicked:' + module._id)
+                                        }
+                                    }>
+                                        <i className="fa fa-edit"></i>
+                                    </a>
+                                }
+                                &nbsp;&nbsp;
+                                <a>
+                                    <i className="fas fa-trash"></i>
+                                </a>
+                                &nbsp;&nbsp;
+                                <a>
+                                    <i className="fas fa-check-circle"></i>
+                                </a>
+                            </span>
+                            </li>
+                        // <ModuleListItem
+                        //     key={module._id}
+                        //     edit={() => {
+                        //         const moduleId = module._id
+                        //         console.log('ModuleListComponent ' + this.props.courseId)
+                        //         this.props.history.push(`/course-editor/${this.props.courseId}/module/${moduleId}`)
+                        //         this.setState({
+                        //             editingModuleId: module._id
+                        //         })
+                        //     }}
+                        //     select={() => {
+                        //         const moduleId = module._id
+                        //         this.props.history.push(`/course-editor/${this.props.courseId}/module/${moduleId}`)
+                        //         this.setState({
+                        //             activeModuleId: module._id
+                        //         })
+                        //     }}
+                        //     save={() => this.setState({
+                        //         editingModuleId: ''
+                        //     })}
+                        //     editing={module._id === this.state.editingModuleId}
+                        //     active={module._id === this.state.activeModuleId}
+                        //     module={module}/>
+                            )
                 }
                 <li className="list-group-item">
                     <button onClick={
@@ -82,3 +116,21 @@ export default class ModuleListComponent extends React.Component {
 //     </Provider>
 //
 // export default ModuleListContainer
+
+const stateToPropertyMapper = (state) => ({})
+const dispatchToPropertyMapper = (dispatch) => ({
+    deleteModule: (moduleId) => {
+        fetch(`${MODULES_API_URL}/${moduleId}`, {
+            method: 'DELETE'
+        }).then(response => response.json())
+            .then(status => dispatch({
+                type: 'DELETE_MODULE',
+                moduleId: moduleId
+            }))
+    }
+})
+
+export default connect(
+    stateToPropertyMapper,
+    dispatchToPropertyMapper
+)(ModuleListComponent)

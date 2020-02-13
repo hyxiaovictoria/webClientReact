b0cd499a3bc88d0ac878d807c5d92b75fc0a56d3
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-02-12 19:36:01 -0800

    Updated topic pills.

--- src/components/CourseEditor/TopicPills.js
+++ src/components/CourseEditor/TopicPills.js
@@ -1,9 +1,33 @@
 import React from "react";
-
+import {connect} from "react-redux";
 import "./CourseEditorComponent.css"
+import {findTopicsForLesson, createTopic, updateTopic, deleteTopic} from "../../services/TopicService";
+import {LESSONS_API_URL, LESSONS_TOPICS_API_URL} from "../../constants";
+import {createLesson, updateLesson} from "../../services/LessonService";
 
 
 class TopicPills extends React.Component {
+
+    componentDidMount() {
+        this.props.findTopicsForLesson(this.props.lessonId)
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(this.props.lessonId !== prevProps.lessonId) {
+            this.props.findTopicsForLesson(this.props.lessonId)
+        }
+    }
+
+    state = {
+        selectedTopicId: '',
+        editingTopicId: '',
+        topic: {
+            title: '',
+            _id: ''
+        }
+    }
+
+
     render() {
         return (
             <div className="form-row">
@@ -25,4 +49,57 @@ class TopicPills extends React.Component {
     }
 }
 
-export default TopicPills
+const stateToPropertyMapper = (state) => ({
+    lessons: state.lessons.lessons
+})
+
+const dispatcherToPropertyMapper = (dispatcher) => ({
+    findTopicsForLesson: lessonId =>
+        fetch(LESSONS_TOPICS_API_URL(lessonId))
+            .then(response => response.json())
+            .then(topics => dispatcher({
+                type: 'FIND_TOPICS_FOR_LESSON',
+                topics: topics
+            }))//,
+    // updateLesson: async (lesson) => {
+    //     const actualLesson = await updateLesson(lesson)
+    //     dispatcher({
+    //         type: 'UPDATE_LESSON',
+    //         lesson: actualLesson,
+    //         lessonId: actualLesson._id
+    //     })
+    // },
+    // addLesson: async (moduleId) => {
+    //     const newLesson = await createLesson(moduleId,
+    //         {title: 'New lesson',
+    //             _id: new Date().getTime()})
+    //     dispatcher({
+    //         type: 'CREATE_LESSON',
+    //         lesson: newLesson,
+    //         lessonId: newLesson._id
+    //     })
+    // },
+    // deleteLesson: (lessonId) =>
+    //     fetch(`${LESSONS_API_URL}/${lessonId}`, {
+    //         method: 'DELETE'
+    //     }).then(response => response.json())
+    //         .then(status =>
+    //             dispatcher({
+    //                 type: 'DELETE_LESSON',
+    //                 lessonId: lessonId
+    //             })),
+    // findAllLessons: () =>
+    //     fetch(LESSONS_API_URL)
+    //         .then(response => response.json())
+    //         .then(lessons =>
+    //             dispatcher({
+    //                 type: 'FIND_ALL_LESSONS',
+    //                 lessons: lessons
+    //             })
+    //         )
+})
+
+export default connect (
+    stateToPropertyMapper,
+    dispatcherToPropertyMapper
+)(TopicPills)

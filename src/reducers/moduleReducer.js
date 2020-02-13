f3b5da98031b5ecd394b430146d1e65e25ea28de
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-02-12 18:55:55 -0800

    Comment something.

--- src/reducers/moduleReducer.js
+++ src/reducers/moduleReducer.js
@@ -1,18 +1,18 @@
 //import {CREATE_MODULE, FIND_MODULES_FOR_COURSE, DELETE_MODULE} from "../actions/moduleActions";
 
 
-let initialState = {
-    modules :
-        [
-            {_id: "123", title: "Module 1-jQuery"},
-            {_id: "234", title: "Module 2-React"},
-            {_id: "345", title: "Module 3-Redux"},
-            {_id: "456", title: "Module 4-Native"},
-            {_id: "678", title: "Module 5-Angular"},
-            {_id: "789", title: "Module 6-Node"},
-            {_id: "890", title: "Module 7-Mongo"}
-        ]
-}
+// let initialState = {
+//     modules :
+//         [
+//             {_id: "123", title: "Module 1-jQuery"},
+//             {_id: "234", title: "Module 2-React"},
+//             {_id: "345", title: "Module 3-Redux"},
+//             {_id: "456", title: "Module 4-Native"},
+//             {_id: "678", title: "Module 5-Angular"},
+//             {_id: "789", title: "Module 6-Node"},
+//             {_id: "890", title: "Module 7-Mongo"}
+//         ]
+// }
 
 // const moduleReducer = (state = initialState, action) => {
     const moduleReducer = (state = {modules:[]}, action) => {

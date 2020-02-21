371a988e9ccf6eaae7b76f40af3b3241b3cb8d33
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-02-21 01:31:25 -0800

    Module list is better, e.g. edit, auto hide.

--- src/components/CourseEditor/ModuleListItem.js
+++ src/components/CourseEditor/ModuleListItem.js
@@ -3,19 +3,28 @@ import {connect} from 'react-redux'
 import {COURSES_MODULES_API_URL, MODULES_API_URL} from "../../constants";
 import "./CourseEditorComponent.css"
 
+
+
 const ModuleListItem = ({save, edit, editing, module, deleteModule, active, select}) =>
     <li
         onClick={select}
         className={`wbdv-module-item ${active ? 'active':''}`}>
+
+        {!editing &&
         <span className="wbdv-module-item-title">
         {module.title}
         </span>
+        }
 
         <span className="fa-right-only-50">
             {!editing && <a onClick={edit}>
                 <i className="fa fa-edit"></i>
             </a>
             }
+            {editing && <input
+                onChange={e => {module.title = e.target.value}}>
+            </input>}
+
             &nbsp;&nbsp;
             {editing &&<a onClick={() =>
                 deleteModule(module._id)}>

f249df261beb1146d83c52fbd6158ce5f6befbdc
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-03-17 02:15:01 -0700

    Better.

--- src/components/CourseEditor/widgets/ImageWidget.js
+++ src/components/CourseEditor/widgets/ImageWidget.js
@@ -5,23 +5,23 @@ const ImageWidget = ({widget , handleChangeImage , preview})=>{
     let input;
     return(
         <div>
-            {/*<div style={preview?{"display": "none"}:{"display": "inline"}}>*/}
-            {/*    <FormControl*/}
-            {/*        className={'my-3'}*/}
-            {/*        componentClass="input"*/}
-            {/*        type="text"*/}
-            {/*        value={widget.src}*/}
-            {/*        // onChange={()=>handleChangeImage(widget.id , input.value)}*/}
-            {/*        // inputRef={(ref) => {input = ref}}*/}
-            {/*        placeholder="Image src"*/}
-            {/*    />*/}
-            {/*    <FormControl*/}
-            {/*        className={'my-3'}*/}
-            {/*        componentClass="input"*/}
-            {/*        type="text"*/}
-            {/*        inputRef={(ref) => {this.input = ref}}*/}
-            {/*        placeholder="Widget Name" />*/}
-            {/*</div>*/}
+            <div style={preview?{"display": "none"}:{"display": "inline"}}>
+                <FormControl
+                    className={'my-3'}
+                    componentClass="input"
+                    type="text"
+                    value={widget.url}
+                    // onChange={()=>handleChangeImage(widget.id , input.value)}
+                    // inputRef={(ref) => {input = ref}}
+                    placeholder="Image src"
+                />
+                <FormControl
+                    className={'my-3'}
+                    componentClass="input"
+                    type="text"
+                    // inputRef={(ref) => {this.input = ref}}
+                    placeholder="Widget Name" />
+            </div>
             <img src={widget.url} width="300" alt="Picture not found"/>
         </div>
     )

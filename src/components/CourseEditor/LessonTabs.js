dc55ad03529196123f4aa306a4d3d443d6e2f56c
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-04-24 18:00:11 -0700

    Formatting.

--- src/components/CourseEditor/LessonTabs.js
+++ src/components/CourseEditor/LessonTabs.js
@@ -36,172 +36,112 @@ class LessonTabs extends React.Component {
     render() {
         return (
             <div className="container-fluid">
-            <div className="row course-manager-header-row">
-                <div className="col-sm-4">
+                <div className="row course-manager-header-row">
+                    <div className="col-sm-4">
                     <span
                         onClick={() => this.props.history.push("/")}
                         className="wbdv-course-editor wbdv-close">
                         <i className="course-manager-header-row black fa fa-times fa-2x fa-inverse"></i>
                     </span>
-                    <span className="course-editor-header-title">
+                        <span className="course-editor-header-title">
                         {this.state.course === undefined ? "" : this.state.course["title"]}
                     </span>
-                </div>
-                <ul className="nav nav-tabs wbdv-page-tab col-sm-5">
-                    {this.props.lessons.map(lesson =>
-                        <li onClick={() => {
-                            this.props.history.push(
-                                `/course-editor/${this.props.courseId}/module/${this.props.moduleId}/lesson/${lesson._id}`)
-                            this.setState({
-                                activeLessonId: lesson._id
-                            })
-                        }
-                        }
-                            key={lesson._id} className="nav-item">
-                            {this.state.editingLessonId !== lesson._id
-                            &&
-                            <span className="wbdv-black">{lesson.title}</span>
-                            }
-
-                            {this.state.editingLessonId === lesson._id
-                            &&
-                            <input
-                                onChange={(e) => {
-                                    const newTitle = e.target.value
-                                    this.setState(prevState => ({
-                                        lesson: {
-                                            ...prevState.lesson,
-                                            title: newTitle
-                                        }
-                                    }))
-                                }}
-                                value={this.state.lesson.title}/>
-                            }
-
-                            {this.state.editingLessonId !== lesson._id
-                            &&
-                            <a onClick={() => {
+                    </div>
+                    <ul className="nav nav-tabs wbdv-page-tab col-sm-5">
+                        {this.props.lessons.map(lesson =>
+                            <li onClick={() => {
+                                this.props.history.push(
+                                    `/course-editor/${this.props.courseId}/module/${this.props.moduleId}/lesson/${lesson._id}`)
                                 this.setState({
-                                    lesson: lesson,
-                                    editingLessonId: lesson._id
+                                    activeLessonId: lesson._id
                                 })
-                            }}>
-                                <i className="fas fa-edit"></i>
-                            </a>
                             }
+                            }
+                                key={lesson._id} className="nav-item">
+                                {this.state.editingLessonId !== lesson._id
+                                &&
+                                <span className="wbdv-black">{lesson.title}</span>
+                                }
 
-                            {this.state.editingLessonId === lesson._id
-                            &&
-                            <a onClick={
-                                () => this.props.deleteLesson(lesson._id)}>
-                                <i className="fas fa-trash"></i>
-                            </a>
-                            }
-                            {this.state.editingLessonId === lesson._id
-                            &&
-                            <a onClick={() => {
-                                this.props.updateLesson(this.state.lesson)
-                                    .then(() =>
-                                        this.setState({
-                                            editingLessonId: ''
-                                        })
-                                    )
-                                console.log('Updated lesson title: ' + this.state.lesson.title)
-                            }
-                            }>
-                                <i className="fas fa-check-circle"></i>
-                            </a>
-                            }
-                            &emsp;&emsp;
-                        </li>
-                    )}
-                </ul>
-                <input
-                    type="text"
-                    placeholder="New Lesson Title"
-                    onChange={e => {
-                        this.setState({newLessonTitle: e.target.value})
-                    }}
-                    value={this.state.newLessonTitle}
-                >
-                </input>
-                <a className="wbdv-new-page-btn col-sm-1"
-                   onClick={() => {
-                       const lesson = {
-                           title: this.state.newLessonTitle,
-                           _id: new Date().getTime()
+                                {this.state.editingLessonId === lesson._id
+                                &&
+                                <input
+                                    onChange={(e) => {
+                                        const newTitle = e.target.value
+                                        this.setState(prevState => ({
+                                            lesson: {
+                                                ...prevState.lesson,
+                                                title: newTitle
+                                            }
+                                        }))
+                                    }}
+                                    value={this.state.lesson.title}/>
+                                }
+
+                                {this.state.editingLessonId !== lesson._id
+                                &&
+                                <a onClick={() => {
+                                    this.setState({
+                                        lesson: lesson,
+                                        editingLessonId: lesson._id
+                                    })
+                                }}>
+                                    <i className="fas fa-edit"></i>
+                                </a>
+                                }
+
+                                {this.state.editingLessonId === lesson._id
+                                &&
+                                <a onClick={
+                                    () => this.props.deleteLesson(lesson._id)}>
+                                    <i className="fas fa-trash"></i>
+                                </a>
+                                }
+                                {this.state.editingLessonId === lesson._id
+                                &&
+                                <a onClick={() => {
+                                    this.props.updateLesson(this.state.lesson)
+                                        .then(() =>
+                                            this.setState({
+                                                editingLessonId: ''
+                                            })
+                                        )
+                                    console.log('Updated lesson title: ' + this.state.lesson.title)
+                                }
+                                }>
+                                    <i className="fas fa-check-circle"></i>
+                                </a>
+                                }
+                                &emsp;&emsp;
+                            </li>
+                        )}
+                    </ul>
+                    <input
+                        type="text"
+                        placeholder="New Lesson Title"
+                        onChange={e => {
+                            this.setState({newLessonTitle: e.target.value})
+                        }}
+                        value={this.state.newLessonTitle}
+                    >
+                    </input>
+                    <a className="wbdv-new-page-btn col-sm-1"
+                       onClick={() => {
+                           const lesson = {
+                               title: this.state.newLessonTitle,
+                               _id: new Date().getTime()
+                           }
+                           console.log("Add new lesson clicked" + JSON.stringify(lesson))
+                           this.props.addLesson(this.props.moduleId, lesson)
                        }
-                       console.log("Add new lesson clicked" + JSON.stringify(lesson))
-                       this.props.addLesson(this.props.moduleId, lesson)
-                   }
 
-                   }>
-                    <i className="fa fa-plus fa-2x fa-inverse"></i>
-                </a>
-            </div>
+                       }>
+                        <i className="fa fa-plus fa-2x fa-inverse"></i>
+                    </a>
+                </div>
             </div>
         )
     }
-
-    // render() {
-    //     return(
-    //         <ul className="nav nav-tabs">
-    //             {
-    //                 this.props.lessons && this.props.lessons.map(lesson =>
-    //                     <li className={`nav-item`}
-    //                         onClick={() => this.setState({
-    //                             selectedLessonId: lesson._id
-    //                         })}
-    //                         key={lesson._id}>
-    //                         <a className={`nav-link
-    //                                         ${(this.state.editingLessonId === lesson._id || this.state.selectedLessonId === lesson._id)?'active':''}`}>
-    //                             {this.state.editingLessonId !== lesson._id &&
-    //                             <span>{lesson.title}</span>}
-    //                             {this.state.editingLessonId === lesson._id &&
-    //                             <input
-    //                                 onChange={(e) => {
-    //                                     const newTitle = e.target.value
-    //                                     this.setState(prevState => ({
-    //                                         lesson: {
-    //                                             ...prevState.lesson,
-    //                                             title: newTitle
-    //                                         }
-    //                                     }))
-    //                                 }}
-    //                                 value={this.state.lesson.title}/>}
-    //                             <button onClick={() =>
-    //                             {
-    //                                 this.props.updateLesson(this.state.lesson)
-    //                                     .then(() =>
-    //                                         this.setState({
-    //                                             editingLessonId: ''
-    //                                         })
-    //                                     )
-    //                             }
-    //                             }>
-    //                                 Save
-    //                             </button>
-    //                             <button onClick={
-    //                                 () => this.props.deleteLesson(lesson._id)}>
-    //                                 X
-    //                             </button>
-    //                             <button onClick={() => {
-    //                                 this.setState({
-    //                                     lesson: lesson,
-    //                                     editingLessonId: lesson._id
-    //                                 })
-    //                             }}>
-    //                                 Edit
-    //                             </button>
-    //                         </a>
-    //                     </li>)
-    //             }
-    //             <li className="nav-item">
-    //                 <button onClick={() => this.props.addLesson(this.props.moduleId)}>+</button>
-    //             </li>
-    //         </ul>
-    //     )
-    // }
 }
 
 
